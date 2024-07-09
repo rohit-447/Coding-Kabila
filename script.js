c21ddbcb29f368c83d0f966edac043837ede77cc
rohit-447 <rohitkumar826344@gmail.com>
2024-07-10 00:01:28 +0530

    nav bar scroll color updated

--- script.js
+++ script.js
@@ -10,15 +10,16 @@ new TypeIt(".element", {
 
 document.addEventListener("DOMContentLoaded", function () {
   var navbar = document.getElementById("navbar");
+  if (!navbar) {
+      console.error("Navbar element not found!");
+      return;
+  }
+
+  var changeColorPosition = 100; // Adjust this value as needed
 
   window.addEventListener("scroll", function () {
-      // Get the current scroll position
       var scrollPosition = window.scrollY;
 
-      // Define the scroll position where the navbar should change color
-      var changeColorPosition = 100; // Adjust this value as needed
-
-      // Toggle the 'scrolled' class based on the scroll position
       if (scrollPosition >= changeColorPosition) {
           navbar.classList.add("scrolled");
       } else {

cd257efcec2d1dfc5ee5058ca042279a55cbd370
rohit-447 <rohitkumar826344@gmail.com>
2024-07-09 22:10:26 +0530

    img opecity removed

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ new TypeIt(".element", {
       color: "blue", // Set the color to blue
   }
 }).go();
+
+document.addEventListener("DOMContentLoaded", function () {
+  var navbar = document.getElementById("navbar");
+
+  window.addEventListener("scroll", function () {
+      // Get the current scroll position
+      var scrollPosition = window.scrollY;
+
+      // Define the scroll position where the navbar should change color
+      var changeColorPosition = 100; // Adjust this value as needed
+
+      // Toggle the 'scrolled' class based on the scroll position
+      if (scrollPosition >= changeColorPosition) {
+          navbar.classList.add("scrolled");
+      } else {
+          navbar.classList.remove("scrolled");
+      }
+  });
+});

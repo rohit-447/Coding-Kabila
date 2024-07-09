f71ab3468b0c5bfd52bb5f88c92fba3b1e532407
rohit-447 <rohitkumar826344@gmail.com>
2024-07-09 21:13:11 +0530

    code added

---
+++ script.js
@@ -0,0 +1,9 @@
+new TypeIt(".element", {
+  strings: ["Coding Kabila", "Welcomes You..."],
+  speed: 50,
+  waitUntilVisible: true,
+  html: true, // Enable HTML for custom formatting
+  css: {
+      color: "blue", // Set the color to blue
+  }
+}).go();
